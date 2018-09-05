03eb789a125c56ec5a373d84610677f852bd11d2
joshuahansen <joshua.hansen188@gmail.com>
2018-09-05 14:24:45 +1000

    Expanded simple register code to include more form fields for patient registration

--- app/forms.py
+++ app/forms.py
@@ -9,18 +9,59 @@
 from flask import request, render_template
 
 from flask_wtf import FlaskForm
-from wtforms import StringField,DateTimeField
+from wtforms import StringField,DateTimeField,SelectField
 from wtforms.validators import DataRequired,Email
 
 class MapsRegister(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    fname = StringField('fname', validators=[DataRequired()])
+    lname = StringField('lname', validators=[DataRequired()])
+    phone = StringField('phone', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(),Email()])
     dob = DateTimeField('dob', format='%d/%m/%Y')
+    gender = SelectField('gender',
+        validators=[DataRequired()],
+        choices=[
+            ('male', 'Male'),
+            ('female', 'Female'),
+            ('other', 'Other')
+        ]
+    )
+    address = StringField('address', validators=[DataRequired()])
+    city = StringField('city', validators=[DataRequired()])
+    state = SelectField('state', 
+        validators=[DataRequired()], 
+        choices=[
+            ('vic', 'VIC'),
+            ('nsw', 'NSW'),
+            ('act', 'ACT'),
+            ('qld', 'QLD'),
+            ('nt', 'NT'), 
+            ('wa', 'WA'),
+            ('sa', 'SA'),
+            ('tas', 'TAS')
+        ]
+    )
+    postcode = StringField('postcode', validators=[DataRequired()])
 
 def maps_register():
     form = MapsRegister()
+    states = {
+        'vic': 'VIC',
+        'nsw': 'NSW',
+        'act': 'ACT',
+        'qld': 'QLD',
+        'nt': 'NT',
+        'wa': 'WA',
+        'sa': 'SA',
+        'tas': 'TAS'
+    }
+    genders = {
+        'male': 'Male',
+        'female': 'Female',
+        'other': 'Other'
+    }
     if form.validate_on_submit():
         return "<h2>Registered Successfully!</h2>"
 
-    return render_template('maps_register.html', form=form)
+    return render_template('maps_register.html', form=form, states=states, genders=genders)
 
