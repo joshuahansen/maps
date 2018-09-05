b793c03b0886b909427504d61786ee973828a6eb
joshuahansen <joshua.hansen188@gmail.com>
2018-09-05 19:01:54 +1000

    fixed lables using WTForm fields

--- app/forms.py
+++ app/forms.py
@@ -13,12 +13,12 @@ from wtforms import StringField,DateTimeField,SelectField
 from wtforms.validators import DataRequired,Email
 
 class MapsRegister(FlaskForm):
-    fname = StringField('fname', validators=[DataRequired()])
-    lname = StringField('lname', validators=[DataRequired()])
-    phone = StringField('phone', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(),Email()])
-    dob = DateTimeField('dob', format='%d/%m/%Y')
-    gender = SelectField('gender',
+    fname = StringField('First Name', validators=[DataRequired()])
+    lname = StringField('Last Name', validators=[DataRequired()])
+    phone = StringField('Phone Number', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    dob = DateTimeField('Date of Birth', format='%d/%m/%Y')
+    gender = SelectField('Gender',
         validators=[DataRequired()],
         choices=[
             ('male', 'Male'),
@@ -26,9 +26,9 @@ class MapsRegister(FlaskForm):
             ('other', 'Other')
         ]
     )
-    address = StringField('address', validators=[DataRequired()])
-    city = StringField('city', validators=[DataRequired()])
-    state = SelectField('state', 
+    address = StringField('Address', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    state = SelectField('State', 
         validators=[DataRequired()], 
         choices=[
             ('vic', 'VIC'),
@@ -41,27 +41,13 @@ class MapsRegister(FlaskForm):
             ('tas', 'TAS')
         ]
     )
-    postcode = StringField('postcode', validators=[DataRequired()])
+    postcode = StringField('Post Code', validators=[DataRequired()])
 
 def maps_register():
     form = MapsRegister()
-    states = {
-        'vic': 'VIC',
-        'nsw': 'NSW',
-        'act': 'ACT',
-        'qld': 'QLD',
-        'nt': 'NT',
-        'wa': 'WA',
-        'sa': 'SA',
-        'tas': 'TAS'
-    }
-    genders = {
-        'male': 'Male',
-        'female': 'Female',
-        'other': 'Other'
-    }
+    
     if form.validate_on_submit():
         return "<h2>Registered Successfully!</h2>"
 
-    return render_template('maps_register.html', form=form, states=states, genders=genders)
+    return render_template('maps_register.html', form=form)
 
