2f13d08c4ca91a0c58a8c1c3f5ddac701bbb0a4e
joshuahansen <joshua.hansen188@gmail.com>
2018-09-21 18:39:57 +1000

    updated structure of project. moved into named directories. Added database tables for Doctor and Patient Notes

---
+++ app/api/__init__.py
@@ -0,0 +1,7 @@
+##
+# Medical Appointment System (MAPS) - IoT Sem2 2018
+# 
+# Provides global access to api classes
+# 
+#
+# Authors: Adam Young, Joshua Hansen, Lohgan Nashm, Zach Wingrave
---
+++ app/database_tables/__init__.py
@@ -0,0 +1,9 @@
+##
+# Medical Appointment System (MAPS) - IoT Sem2 2018
+# 
+# This file initialises the database_tables and makes it accessable to the api's
+#
+# Authors: Adam Young, Joshua Hansen, Lohgan Nash, Zach Wingrave
+##
+from app import app
+
