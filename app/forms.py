cbe4896592083277aedfbb8884bd10462437feea
Adam Young <adamyoung012@gmail.com>
2018-10-07 20:37:05 +1100

    Implemented doctor, date and time selection

--- app/forms.py
+++ app/forms.py
@@ -9,15 +9,19 @@
 from flask import redirect, request, render_template
 
 from flask_wtf import FlaskForm
-from wtforms import StringField,DateField,SelectField
+from wtforms import StringField,DateField,DateTimeField,SelectField
 from wtforms.validators import Email,DataRequired
 
 import requests
+from datetime import datetime, timedelta
 
 class AppointmentForm(FlaskForm):
-
     doctor = SelectField('Doctor', validators=[DataRequired()])
-    date = DateField('Desired Appointment Date', format='%d/%m/%Y')
+    date = DateTimeField('Desired Appointment Date', format='%d/%m/%Y')
+
+class ApptWithTime(AppointmentForm):
+    time = SelectField('Time', validators=[DataRequired()])
+
 
 class MapsRegister(FlaskForm):
     fname = StringField('First Name', validators=[DataRequired()])
@@ -60,13 +64,39 @@ def maps_register():
 
 def maps_appointment(config):
     form = AppointmentForm()
+    formwithtime = ApptWithTime()
+    timeArr = []
 
     form.doctor.choices = doctors_list(config['MAPS_API_BASE_URL'])
+    formwithtime.doctor.choices = doctors_list(config['MAPS_API_BASE_URL'])
 
     if form.validate_on_submit():
-        return "Valid"
+        print("Valid")
 
-    return render_template('patient.html', form=form)
+
+        print(form.doctor.data)
+        doctor = form.doctor.data
+        date = form.date.data
+
+        formwithtime.time.choices = times_list(config['MAPS_API_BASE_URL'], doctor, date)
+
+        # Use values given to us previously
+        formwithtime.doctor.default = doctor
+        formwithtime.date.default = date
+
+        # Disabled the fields above time
+        formwithtime.doctor.render_kw={'disabled':''}
+        formwithtime.date.render_kw={'disabled':''}
+
+        return render_template('patient.html', form=formwithtime)
+
+    elif formwithtime.validate_on_submit():
+        # make the appointment
+
+        return render_template('patient.html', form=formwithtime)
+    else:
+        return render_template('patient.html', form=form)
+
 
 # UTILS
 def doctors_list(apiurl):
@@ -78,3 +108,32 @@ def doctors_list(apiurl):
         doctor_list.append((str(doc['id']), fullname))
 
     return doctor_list
+
+def times_list(apiurl, doctor, date):
+    appt_length = 30*60 # seconds
+    time_list = []
+
+    datestr = datetime.strftime(date, "%Y-%m-%d")
+    r = requests.post(apiurl + "/patient/availability/", json={'doctorID': doctor, 'date': datestr})
+    summary = r.json()
+
+    day_start = summary['availability']['startTime']
+    day_end = summary['availability']['endTime']
+
+    start_hr,start_min = map(int, day_start.split(':'))
+    end_hr,end_min = map(int, day_end.split(':'))
+
+    seconds_avail = (int(end_hr) - int(start_hr)) * 60 * 60 + (int(end_min) - int(start_min)) * 60
+    possible_slots = int(seconds_avail / appt_length)
+
+    startTime = date + timedelta(seconds=start_hr*60*60 + start_min*60)
+
+    for i in range(0, possible_slots):
+        appt_time = startTime + timedelta(seconds=i * appt_length)
+        time_list.append((str(i), appt_time.strftime('%H:%M')))
+
+    # for time in r.json():
+    #print(r.json())
+    print(time_list)
+
+    return time_list
